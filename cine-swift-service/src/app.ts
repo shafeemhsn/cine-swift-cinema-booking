3cbe583515c62e1cc926e9aa536e1f340c07d419
Shafee Muhsin <shafeem@fortude.co>
2025-06-06 13:12:15 +0530

    implement routes

--- cine-swift-service/src/app.ts
+++ cine-swift-service/src/app.ts
@@ -2,7 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
-// import routes from "./routes";
+import routes from "./routes";
 import { errorHandler } from "./middleware/errorHandler";
 
 dotenv.config();
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(cors());
 
-// app.use(routes);
+app.use(routes);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   errorHandler(err, req, res, next);
