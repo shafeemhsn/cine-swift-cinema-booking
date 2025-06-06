6d1765fc0e688f98735a958cc34505e75842631d
Shafee Muhsin <shafeem@fortude.co>
2025-06-06 14:44:48 +0530

    db config

--- cine-swift-service/src/server.ts
+++ cine-swift-service/src/server.ts
@@ -1,10 +1,20 @@
 import dotenv from "dotenv";
+
 import { app } from "./app";
+import { AppDataSource } from "./config/postgres-db";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running at port: ${PORT}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database connection established");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running at port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error during database initialization:", error);
+  });
